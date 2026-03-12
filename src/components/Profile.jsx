ef3ca383cc01b5e1b311b5fdf259eec40288c5d8
krishna sekhar <krishna@hyperx2.ai>
2026-03-12 13:03:07 +0530

    fixes:store

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,25 +1,10 @@
-import React, { useEffect, useState } from 'react'
 import EditProfile from './EditProfile'
 import { useSelector } from 'react-redux'
 import axios from 'axios'
 import { BASE_URL } from '../utils/constants'
 
 function Profile() {
-    // const userData = useSelector(store => store.user)
-    const [data, setData] = useState("");
-    const fetchProfile = async () => {
-        try {
-            const res = await axios.get(BASE_URL + "/profile/view", {
-                withCredentials: true
-            });
-            setData(res.data);
-        } catch (err) {
-            console.log(err);
-        }
-    }
-    useEffect(() => {
-        fetchProfile();
-    }, [])
+    const data = useSelector(store => store?.user?.user?.data)
 
     return (
         <div className='flex justify-center my-10'><EditProfile userData={data} /></div>
