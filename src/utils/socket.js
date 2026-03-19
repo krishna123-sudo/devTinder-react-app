d958771beb959fc1a7112c1af0eb6587e5ba8b1a
krishna sekhar <krishna@hyperx2.ai>
2026-03-19 17:27:58 +0530

    socker fixes

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -2,6 +2,12 @@ import { BASE_URL } from "./constants";
 
 import io from "socket.io-client";
 
+// export const createSocketConnection = () => {
+//     return io(BASE_URL)
+// }
 export const createSocketConnection = () => {
-    return io(BASE_URL)
-}
+    return io(BASE_URL, {
+        path: "/socket.io",
+        transports: ["websocket"], // optional but recommended
+    });
+};
